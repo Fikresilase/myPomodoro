b1f3577bd9409a29d8b07f48e433dc1feda7bc1a
Fikresilase <shitubilbila@gmail.com>
2024-12-25 00:39:04 +0300

    enogh for the day am feeling sleepy

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ function pomodoroTimer() {
     // Get the current date and time inside the function
     var pomodoroTime = new Date();
     alert("Button clicked at: " + pomodoroTime.toLocaleString());
+    setTimeout(() => {
+        alert("now take a rest: " + new Date(pomodoroTime.getTime() + 1800000));
+    }, 2000); // This will show the alert after 2 seconds
+    
 }
 
 // Assign the function to the onclick event
