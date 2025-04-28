0eaaf37cc76cf9dfcc14e49747f40eed4a0b3287
Fikresilase <shitubilbila@gmail.com>
2025-04-28 11:52:25 +0300

    pomodoro time loop first draft perfectly working

--- pomodoro.js
+++ pomodoro.js
@@ -1,15 +1,37 @@
 const pomodoro=document.getElementById("pomodoro");
 pomodoro.addEventListener("click",function(){
   alert("Pomodoro timer started!");
+  const initialconfirm=confirm("Do you want to start the pomodoro timer?");
+  if(initialconfirm==true){
+    alert("Pomodoro timer started!");
+    workingTime();
+  }
+  else{
+    alert("Pomodoro cancelled! Goodbye!");
+  }
 })
 function workingTime(){
   setTimeout(() => {
-    alert("pomodoro ended, Time to take a break!");
+    const continueRest=confirm("pomodoro ended, Time to take a break!");
+    if(continueRest==true){
+      alert("Break time started!");
+      breakTime();
+    }
+    else{
+      alert("Pomodoro cancelled! Goodbye!");
+    }
   }, 5000);
 }
 
 function breakTime(){
   setTimeout(() => {
-    alert("Break time ended, Time to get back to work!");
+    const continueWorking=confirm("Break time ended, Time to get back to work!");
+    if(continueWorking==true){
+      alert("Back to work!");
+      workingTime();
+    }
+    else{
+      alert("Pomodoro cancelled! Goodbye!");
+    }
   }, 5000);
 }
